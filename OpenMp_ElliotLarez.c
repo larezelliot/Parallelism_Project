62ff4750e4413227f43a70b4c5c92dad8d6fd477
Elliot Larez <61991911+larezelliot@users.noreply.github.com>
2023-11-10 23:39:51 -0500

    Add files via upload

--- OpenMp_ElliotLarez.c
+++ OpenMp_ElliotLarez.c
@@ -4,8 +4,6 @@
 #include "time.h"
 #include <stdlib.h>
 
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
-
 #define FactorIntToDouble 1.1; 
 
 
@@ -43,6 +41,34 @@ void matrixMultiplication()
 }
 
 
+//Calculates the partial product of A, B and stores it partial C, defined by the thread running using block otimization
+void matrixMultiplication_block(int block_size)
+{
+	
+	if(block_size == N){
+		matrixMultiplication();
+		return;
+	}
+
+	if (threads != omp_get_num_threads())
+		exit(-1);
+
+	int thread_rank = omp_get_thread_num();
+	
+	printf("\tThread: %d of %d started\n", thread_rank, threads);
+	for (int i2 =0; i2 < N; i2 += block_size)
+		for (int j2 =0; j2 < N; j2 += block_size)
+			for (int k2 =0; k2 < N; k2 += block_size){
+				for (int i = i2 + thread_rank; i < i2 + block_size && i < N; i = i+threads)
+					for (int j = j2; j < j2 + block_size && j2 < N; j++)
+						for (int k = k2 ; k < k2 + block_size && k < N ; k++)		
+							C[i][j] += A[i][k] * B[k][j] ;
+			}
+	
+	printf("\tThread: %d of %d ended\n", thread_rank, threads);
+}
+
+
 // Fills A, B with random values between [0-9]
 void matrixInit()
 {
@@ -68,16 +94,18 @@ int main(int argc, char *argv[])
 {
 	
 	N = atoi(argv[1]);
-	threads = atoi(argv[2]);	
+	threads = atoi(argv[2]);
+	int block_size = atoi(argv[3]);	
 	printf("Matrix Dimentions N*N: %d*%d\n", N, N);
 	printf("Number of threads = %d\n", threads);
+	printf("Block Size = %d\n", block_size);
 	
 	matrixInit();
 
 	double t1 = omp_get_wtime();
 	
 	#pragma omp parallel num_threads(threads)
-	matrixMultiplication();
+	matrixMultiplication_block(block_size);
 
 	double t2 = omp_get_wtime();
 	
